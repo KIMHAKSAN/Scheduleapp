3b16f1f0c35fa6cb1cb94dc4eeaa8194f2ac30d4
UK <hwangwonuk94@gmail.com>
2024-08-27 03:03:34 +0900

    doc: 주석 오타 수정

--- src/main/java/com/example/springbasiclayered/service/MemoServiceImpl.java
+++ src/main/java/com/example/springbasiclayered/service/MemoServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Annotation @Service는 @Componet와 같다, Spring Bean으로 등록한다는 뜻.
+ * Annotation @Service는 @Component와 같다, Spring Bean으로 등록한다는 뜻.
  * Spring Bean으로 등록되면 다른 클래스에서 주입하여 사용할 수 있다.
  * 명시적으로 Service Layer 라는것을 나타낸다.
  * 비지니스 로직을 수행한다.
